436b90e2efb7ffa3d6f28a2669e6226062e567b6
rwadhawan03 <85130660+rwadhawan03@users.noreply.github.com>
2022-07-11 16:28:15 -0400

    Delete javascript directory

--- javascript/main.js
+++
@@ -1,4 +0,0 @@
-function feedback() {
-    var x = document.getElementById("myText").value;
-    document.getElementById("feedback").innerHTML = "Thanks for visiting my landing page " + x + ". I appreciate the feedback and I hope to hear from you soon via email!";
-  }
