166e6eaa79855b6e075736583ffb42697ecf5632
rwadhawan03 <85130660+rwadhawan03@users.noreply.github.com>
2022-07-11 16:29:07 -0400

    Add files via upload

---
+++ javascript/main.js
@@ -0,0 +1,4 @@
+function feedback() {
+    var x = document.getElementById("myText").value;
+    document.getElementById("feedback").innerHTML = "Thanks for visiting my landing page " + x + ". I appreciate the feedback and I hope to hear from you soon via email!";
+  }
